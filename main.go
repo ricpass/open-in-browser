2dda190ff5f393feae7d0ac3162d63c221b27b01
ricpass <ricpass@users.noreply.github.com>
2020-12-11 18:46:35 +0000

    first version

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path[1:]
+
+		if !strings.HasPrefix(path, "http://") && strings.HasPrefix(path, "https://") {
+			fmt.Fprintf(w, "INVALID_URL: %s", path)
+			return
+		}
+
+		err := exec.Command("open", path).Run()
+
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err)
+		} else {
+			fmt.Fprintf(w, "OK")
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+}
